0662f154a71a79e4e7448907f6f787ae959dfeb6
rafael <rafaelavelino0908@gmail.com>
2025-03-06 14:58:27 -0300

    Update Script iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 if [[ $1 == "start" ]]; then
-       	
-	cd provedor
-	docker compose up --build &
-	cd ../clientes/pitangui
-	docker compose up --build &
-	cd ../pontanegra
-	docker compose up --build &
+    # Check if network exists, create if not
+    if ! docker network inspect praia-net-proxy >/dev/null 2>&1; then
+        echo "Creating praia-net-proxy network..."
+        docker network create praia-net-proxy
+    else
+        echo "Network praia-net-proxy already exists."
+    fi
+
+    cd provedor
+    docker compose up --build &
+    cd ../clientes/pitangui
+    docker compose up --build &
+    cd ../pontanegra
+    docker compose up --build &
 
 elif [[ $1 == "stop" ]]; then
 
-	cd provedor
-	docker compose down &
-	cd ../clientes/pitangui
-	docker compose down &
-	cd ../pontanegra
-	docker compose down &
+    cd provedor
+    docker compose down &
+    cd ../clientes/pitangui
+    docker compose down &
+    cd ../pontanegra
+    docker compose down &
 
 fi
